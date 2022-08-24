8e57b8cc7a1bb83910b33103e1823056560b83cd
vantoan2412 <68320460+vantoan2412@users.noreply.github.com>
2022-08-24 15:39:49 +0700

    update icon

--- assets/icon/IconSignin.js
+++ assets/icon/IconSignin.js
@@ -4,12 +4,23 @@ function IconSignin(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="100.000000pt"
-      height="100.000000pt"
-      viewBox="0 0 100.000000 100.000000"
+      x="0px"
+      y="0px"
+      width="18px"
+      height="18px"
+      viewBox="0 0 18 18"
+      xmlSpace="preserve"
       {...props}
-    />
+    >
+      <image
+        width={18}
+        height={18}
+        x={0}
+        y={0}
+        href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAQAAADa613fAAAABGdBTUEAALGPC/xhBQAAACBjSFJN AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN RQfmCBgMFTXBCL3dAAAEpElEQVR42u2bzXnqOBSGv5ln9uMOrlPBKBVEVIBTAbDVJlBBoILA5myB CkIqwKkgTgXXHYxLmIVzITgcWX/+uRl/qzySJfTmSPKRzjEwqF/6o/mfIAmgUNlvCUICEncQiC+K M2R4Rary3wCEIswxqQBUlWGDgyp6C0IRnjA1fLjAButwMAFBaI5HRFZNcizUoVcgFOEZ0qnpDosQ dgkCQgJHS1t8VoaRP8qfnWMAAm8kfEfhbRFvjFIFbvys4mkRioNgABGO5NWP79R6DoIBAAJPnYHQ EiIQBgBMKXFv7LFGSOCt9qEUrx9//Q1Zi+2xUv5yB6mZCjlWVTeEYkzxoJmMEeZYug3G2SIkcdRU r7G6/r+lCFskbDtnm7ivkYlmMDPFvq1Voe6xYNtGmLsNx9EiFOFftnKk0tr2c3Zi5urGZUSuFknY mkU9BqDWODBVsdtb3hVkzJSnam3YwwwFU5O0CSKZ8pVpB6rAhqm6aw2EYmYLzU2m1Ulrply0BsIe ZPc2naiCWSeRi9cVFiS17OedKRddg+SW/diCBwdh1MQ1TycgXSooCEnLBqJrkJQpjy37+cGU522B cBpbPp9cL3ZZa04g7GsvsXkDkGQsmLmMydUi3I/NLfp4tOy7EZCUG5yp70oJ66+9tAnCOyNbk+lF Alu2Mm0RRGXsziLqb6go1lwj7do+6vIOu8CRYg2GxJtmo7ZyPM9yv3yI8FNzH8JEPyjGozaCkqpR yyDac/cvmANekJY4FENiXHv6MzjvBwcB6C3oTSOwUzPXpn5vduefvapcc03ULIjKgqLc+wQWPH0t tcMuEMbMLxIfIPRGW+NIrg5j59dBmBiiL4o3RiA3Xs08lmmBe3+MgHF2Etg6bMYpZmFO+mEzH5ba 6EdVOVYhbNEAiGEmSnCIBkA+cBKMwZ//UuzDJz01mK9FMeKLw1OBDFnYnKBBgwb979TArkURBH4F 5z6H0Qq8o9y74HoObByEIghI/KhsuDoVyJDjPdSW7AnCpMXaKkeKd6Q+r0nXhIEYCe4ggyU5lSpw wKtbIq01CAlMkHhaoE4Z9jjYecUWIBRjauQQhsQxvnc0BKEED47psL7aYW+yx9WCUIQpHlq0wzVl 2NQ5/TUglkelJlVzgtGA0BSPHVuiqozPPWJAKMa2ozVRJyYF/SqIQ7o+p+JTIE0GQilw/9UuX0Cs Pp34qtLtyJEjv/4eIIEIMWLcIfaYuCu11IJQhKPDpU7pYmT2riBJSPzj5CFUbu4vQBwwSpcitx5G FUhAYmw5+S5QLkHM4x0FDngJ9RHL6fcjJAbBoKson0DoyTBKnmIf+gupCs4UE8N/6enW+ARSk5Bc qsAOm3aSmUhiYrDpFLgtx3MG+VmzhwT+uMsIpi50Cpym1wcIJXjuF4QFzI3Kz2GFiebBFLdq2dUN ocrVDCNtfkoCnEEk+9hCjbpM8QMAlapbTYhvfAJh83iBmXFmddMwfDBJnEDYZb4Oe/XvibJm0m2i Mwgnp4SjBvXKVw1Zpn3TANI3fRsQ/ed7E+vc6mal+URGDzLteuTm+jZTawDpmwaQvmkA6ZsGkL7p 24AM6pv+A4XGWtq6UKoXAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIyLTA4LTI0VDEwOjIxOjUzKzAy OjAw0sCBTAAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMi0wOC0yNFQxMDoyMTo1MyswMjowMKOdOfAA AAAZdEVYdFNvZnR3YXJlAEFkb2JlIEltYWdlUmVhZHlxyWU8AAAAAElFTkSuQmCC"
+      />
+    </svg>
   )
 }
 
-export default IconSignin;
+export default IconSignin
